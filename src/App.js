7e5c4b346279132a577807cb998d23717693685a
Gagan Arora <arorag2577@gmail.com>
2024-03-02 17:57:29 +0530

    qr code basics

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import QRCode from "react-qr-code";
 
 function App() {
+  const [value, setValue] = useState();
+  const [back, setBack] = useState("#FFFFFF");
+  const [fore, setFore] = useState("#000000");
+  const [size, setSize] = useState(256);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row text-center mt-3">
+        <div className="col-md-12">
+          <h4>
+            This App was build by{" "}
+            <a href="https://gagan-redirect.netlify.app">Gagan Arora</a>
+          </h4>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-md-6 mt-5">
+          <input
+            className="w-100 form-control"
+            type="text"
+            onChange={(e) => setBack(e.target.value || "#FFFFFF")}
+            placeholder="Background color (optional)"
+          />
+        </div>
+        <div className="col-md-6 mt-5">
+          <input
+            className="w-100 form-control"
+            type="text"
+            onChange={(e) => setFore(e.target.value)}
+            placeholder="Foreground color (optional)"
+          />
+        </div>
+      </div>
+      <div className="row mt-3">
+        <input
+          className="w-100 form-control"
+          type="text"
+          onChange={(e) => setValue(e.target.value)}
+          placeholder="Value of QR code"
+        />
+      </div>
+      <div className="row mt-5 qr-container">
+        {value && (
+          <QRCode
+            title="Your QR code is ready!"
+            value={value}
+            bgColor={back}
+            fgColor={fore}
+            size={size === "" ? 300 : size}
+          />
+        )}
+      </div>
     </div>
   );
 }
